222acc6259b15e6e7538f84b5dca611249865135
Annie <engtianyufan@gmail.com>
2019-06-17 16:39:39 -0400

    deleted extraneous

--- TreeNode.py
+++ TreeNode.py
@@ -22,14 +22,3 @@ class TreeNode:
             print(current_node.value)
             nodes_to_visit += current_node.children
 
-
-root = TreeNode("CEO")
-first_child = TreeNode("Vice-President")
-second_child = TreeNode("Head of Marketing")
-third_child = TreeNode("Marketing Assistant")
-
-root.add_child(first_child)
-root.add_child(second_child)
-second_child.add_child(third_child)
-
-root.traverse()
